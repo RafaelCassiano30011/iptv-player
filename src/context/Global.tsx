e44df4466d978d269fe7b96d31a5ec63fa8077c5
rafael <rafael30011@gmail.com>
2024-11-26 01:24:52 -0300

    style login

--- src/context/Global.tsx
+++ src/context/Global.tsx
@@ -34,12 +34,10 @@ export default function GlobalProvider({ children }: Props) {
 
   const removeSelectStream = () => setSelecteStream(undefined);
 
-  // Atualiza o localStorage sempre que as categorias mudam
   useEffect(() => {
     localStorage.setItem("categories", JSON.stringify(categories));
   }, [categories]);
 
-  // Atualiza o localStorage sempre que os streams mudam
   useEffect(() => {
     localStorage.setItem("allStreams", JSON.stringify(allStreams));
   }, [allStreams]);
@@ -48,7 +46,6 @@ export default function GlobalProvider({ children }: Props) {
     const ONE_HOUR = 60 * 60 * 1000;
     const lastFetchTimeStreams = localStorage.getItem("lastFetchTimeStreams");
 
-    // Verifica se já passou uma hora desde a última requisição de streams
     if (allStreams.length > 0 && lastFetchTimeStreams && Date.now() - parseInt(lastFetchTimeStreams, 10) < ONE_HOUR) {
       console.log("Ignorando atualização de streams.");
       return;
@@ -56,10 +53,13 @@ export default function GlobalProvider({ children }: Props) {
 
     const init = async (type: Type, categoryId: string) => {
       const data = await getStreams({ type, category_id: categoryId });
-      setAllStreams((prevStreams) => ({
-        ...prevStreams,
-        ...data,
-      }));
+      // Adiciona novos streams e ordena o array
+      setAllStreams((prevStreams) => {
+        const updatedStreams = [...prevStreams, ...data];
+        // Ordena os streams por nome (ou outro critério desejado)
+        updatedStreams.sort((a, b) => a.name.localeCompare(b.name));
+        return updatedStreams;
+      });
     };
 
     const processStreams = async (types: Type[]) => {
@@ -74,12 +74,10 @@ export default function GlobalProvider({ children }: Props) {
     if (Object.keys(categories).length === 3) processStreams(types);
   }, [categories, allStreams]);
 
-  // Efeito para gerenciar as categorias
   useEffect(() => {
     const ONE_HOUR = 60 * 60 * 1000;
     const lastFetchTimeCategories = localStorage.getItem("lastFetchTimeCategories");
 
-    // Verifica se já passou uma hora desde a última requisição de categorias
     if (categories && lastFetchTimeCategories && Date.now() - parseInt(lastFetchTimeCategories, 10) < ONE_HOUR) {
       console.log("Ignorando atualização de categorias.");
       return;
@@ -93,12 +91,10 @@ export default function GlobalProvider({ children }: Props) {
     const processCategories = async (types: Type[]) => {
       await Promise.all(types.map((type) => getCategoriesInit(type)));
 
-      // Atualiza o timestamp da última execução para categorias
       localStorage.setItem("lastFetchTimeCategories", Date.now().toString());
       getAllStreams();
     };
 
-    // Processa as categorias
     processCategories(types);
   }, []);
 
--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -33,20 +33,24 @@ export default function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-      <input
-        onChange={(e) => setFormState({ ...formState, username: e.target.value })}
-        value={username}
-        placeholder="Usuario"
-      />
-      <input
-        onChange={(e) => setFormState({ ...formState, password: e.target.value })}
-        value={password}
-        placeholder="Senha"
-        type="password"
-      />
+    <div className="w-full h-full flex items-center justify-center p-10">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5 max-w-[300px] w-full">
+        <input
+          onChange={(e) => setFormState({ ...formState, username: e.target.value })}
+          value={username}
+          placeholder="Usuario"
+          className="pl-3 py-1 w-full outline-none border-2 border-transparent focus:border-cyan-500 rounded-lg transition-all font-bold text-base text-white bg-neutral-800"
+        />
+        <input
+          onChange={(e) => setFormState({ ...formState, password: e.target.value })}
+          value={password}
+          placeholder="Senha"
+          type="password"
+          className="pl-3 py-1 w-full outline-none border-2 border-transparent focus:border-cyan-500 rounded-lg transition-all font-bold text-base text-white bg-neutral-800"
+        />
 
-      <button type="submit">teste</button>
-    </form>
+        <button className="w-full py-1 text-base bg-cyan-500 hover:bg-cyan-700 transition-all rounded-lg font-bold text-white" type="submit">Logar</button>
+      </form>
+    </div>
   );
 }
