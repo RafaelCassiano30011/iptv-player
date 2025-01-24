e8afe057ba8b4989cf6ac9482f00dd7b24852844
Rafael <rafael30011@gmail.com>
2025-01-24 01:51:48 -0300

    initial novamente

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/", // Endpoint do proxy
+  baseURL: "http://localhost:3000", // Endpoint do proxy
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 const proxy = async (url: string) => {
-  return await api.post("/proxy", { url });
+  return await api.post("/proxy", { request: url });
 };
 
 export { api, proxy };
